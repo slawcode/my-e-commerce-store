53ff6ae1ef2cb191a286bee708b1acf9b192f538
Sheryl L <s_lawrence0@yahoo.ca>
2024-04-02 19:46:03 -0400

    columns defined in tag.js and comments added

--- models/Tag.js
+++ models/Tag.js
@@ -6,7 +6,16 @@ class Tag extends Model {}
 
 Tag.init(
   {
-    // define columns
+    // Tag columns defined 
+    id: {
+      type: DataTypes.INTEGER, // Set integer as the data type 
+      allowNull: false, // Do not allow null values
+      primaryKey: true, // Set as the primary key 
+      autoIncrement: true, // Auto increment values for new records
+    },
+    tag_name: {
+      type: DataTypes.STRING, // Set integer as the data type
+    },
   },
   {
     sequelize,
