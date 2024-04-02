fa4d7675150548a8477d797ea6b7800bd677a2e3
Sheryl L <s_lawrence0@yahoo.ca>
2024-04-02 17:54:44 -0400

    colums defined in category.js and comments added

--- models/Category.js
+++ models/Category.js
@@ -6,10 +6,22 @@ class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    // Id column defined
+    id: {
+      type: Datatypes.INTEGER, // Set integer as the data type 
+      allowNull: false, // Do not allow null values 
+      primaryKey: true, // Set as the primary key
+      autoIncrement: true, // Auto increment values for new records
+    },
+    // Category_name colum defined
+    category_name: {
+      type: DataTypes. STRING, // Set string as the data type
+    }
   },
   {
+    // Link to database connection
     sequelize,
+    // Set to false to remove 'created_at' and 'updated_at' fields
     timestamps: false,
     freezeTableName: true,
     underscored: true,
