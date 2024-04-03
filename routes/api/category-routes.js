3334b88c1786e92d0c1f07cfc80a44be48dcfd14
Sheryl L <s_lawrence0@yahoo.ca>
2024-04-03 17:51:51 -0400

    router.get code work and comments added in category-routes.js

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,45 +3,51 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // Find all categories
-  // Be sure to include its associated Products
+// Find all categories
+router.get('/', async (req, res) => {
+  try {
+    const allCategories = await Category.findAll({
+      include: [{ model: Product}],
+    });
+    res.status(200).json(allCategories);
+  } catch (err) { // If there is an error, send a 500 status 
+    res.status(500).json(err);
+  }
 });
 
+// Find one category by its `id` value
 router.get('/:id', (req, res) => {
-  // Find one category by its `id` value
+  
   // Be sure to include its associated Products
 });
 
+// Create a new category
 router.post('/', (req, res) => {
-  // Create a new category
+  
 
 });
 
+// Update a category by its `id` value
 router.put('/:id', (req, res) => {
-  // Update a category by its `id` value
+  
 
 });
 
+// Delete a category by its `id` value
 router.delete('/:id', async (req, res) => {
-  // Delete a category by its `id` value
   try { 
-    // Deletes a category by its matching id value
-    const deleteCategory = await Category.destroy({
+    const deleteCategory = await Category.destroy({ // Deletes a category by its matching id value
       where: {
         id: req.params.id
       }
     });
-    
     // If the category is not found, send a 404 status with a  message and if not return the deleted data
     if (!deleteCategory) {
       res.status(404).json({ message: 'No Category found with this id'});
       return;
     }
     res.status(200).json(deleteCategory);
-    
-    // If there is an error, send a 500 status 
-  } catch (err) {
+  } catch (err) { // If there is an error, send a 500 status 
     res.status(500).json(err);
   }
 });
