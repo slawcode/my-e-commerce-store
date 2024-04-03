13e72133517f391d2ae8f5aefabb16d1ec8233c1
Sheryl L <s_lawrence0@yahoo.ca>
2024-04-03 18:20:43 -0400

    router.post code work and comments added in category-routes.js

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -22,9 +22,14 @@ router.get('/:id', (req, res) => {
 });
 
 // Create a new category
-router.post('/', (req, res) => {
-  
-
+router.post('/', async (req, res) => {
+  try {
+    const newCategory = await Category.create(req.body);
+    res.status(200).json(newCategory);
+  } catch (err) // If there is an error, send a 400 status
+ {
+  res.status(400).json(err); 
+ }
 });
 
 // Update a category by its `id` value
